00561d8b5f12a64ffff8037af7740d0a67a2307f
Sajay1 <sajaysuresh307@gmail.com>
2026-02-24 13:21:32 +0530

    navbar

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,12 @@
 export default function Home(){
 
+
     return(
         <>
-        <h1>HI</h1>
+        <div className="bg-[url(/assets/gabriel_ghnassia.jpeg)]"></div>
+        <h1>WELCOME TO LUXURY </h1>
+        <h2>HOTELS</h2>
+        <h3>Book your stay and enjoy Luxury redefined at the most affordable rates.</h3>
         </>
     )
 }
