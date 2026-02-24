39b88151f09ed5dfc0ec3cc975644d7e2ccf54fd
Sajay1 <sajaysuresh307@gmail.com>
2026-02-24 14:02:43 +0530

    home page updates

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,10 +3,14 @@ export default function Home(){
 
     return(
         <>
-        <div className="bg-[url(/assets/gabriel_ghnassia.jpeg)]"></div>
-        <h1>WELCOME TO LUXURY </h1>
-        <h2>HOTELS</h2>
-        <h3>Book your stay and enjoy Luxury redefined at the most affordable rates.</h3>
+        <div className="flex flex-col justify-center text-white items-center bg-[url('https://media.istockphoto.com/id/119926339/photo/resort-swimming-pool.jpg?s=612x612&w=0&k=20&c=9QtwJC2boq3GFHaeDsKytF4-CavYKQuy1jBD2IRfYKc=')] bg-cover bg-center min-h-screen min-w-full ">
+        <div className="mr-100 mb-80 pt-30 font-averia ">
+            <p className="text-[50px]">WELCOME TO  </p>
+        <p className="text-[118px]">LUXURY</p>
+        <p className="text-[80px]">HOTELS</p>
+        <p className="text-[25px]">Book your stay and enjoy Luxury redefined at the most affordable rates.</p>
+        </div>
+        </div>
         </>
     )
 }
