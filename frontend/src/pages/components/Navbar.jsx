24f82e8e2dcda439fb2ad79b7c5003d3c3c4f3d6
Sajay1 <sajaysuresh307@gmail.com>
2026-03-02 15:07:20 +0530

    ...

--- frontend/src/pages/components/Navbar.jsx
+++ frontend/src/pages/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { ReactTyped } from "react-typed";
 import { useState } from "react";
 import Links from "../components/Links"
 export default function Navbar() {
+  
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   const linkStyle =
